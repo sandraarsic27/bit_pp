bb13b54180c05608830ba86ec44419398107d1fa
BIT Student <classroom@bgit.rs>
2019-02-12 17:10:14 +0100

    poruka

---
+++ switch/1switch .js
@@ -0,0 +1,36 @@
+var a = "1";
+
+switch (a) {
+    case '1':
+        console.log("Ponedeljak");
+        break;
+
+    case '2':
+        console.log("Utorak");
+        break;
+
+    case '3':
+        console.log("Sreda");
+        break;
+
+    case '4':
+        console.log("Cetvrtak");
+        break;
+
+    case '5':
+        console.log("Petak");
+        break;
+
+    case '6':
+        console.log("Subota");
+        break;
+
+    case '7':
+        console.log("Nedelja");
+        break;
+
+    default:
+        console.log("Niste uneli dobar broj");
+        break;
+
+}
