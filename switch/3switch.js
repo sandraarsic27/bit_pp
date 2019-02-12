dcd926a68ab65acb7eff7b2935166548e6b411eb
BIT Student <classroom@bgit.rs>
2019-02-12 17:21:29 +0100

    3.Swiitch

---
+++ switch/3switch.js
@@ -0,0 +1,43 @@
+var a = "1";
+
+switch (a) {
+    case '1':
+        console.log("Ponedeljak");
+        console.log("Nije vikend :(")
+        break;
+
+    case '2':
+        console.log("Utorak");
+        console.log("Nije vikend :(")
+        break;
+
+    case '3':
+        console.log("Sreda");
+        console.log("Nije vikend :(")
+        break;
+
+    case '4':
+        console.log("Cetvrtak");
+        console.log("Nije vikend :(")
+        break;
+
+    case '5':
+        console.log("Petak");
+        console.log("Nije vikend :(")
+        break;
+
+    case '6':
+        console.log("Subota");
+        console.log("Vikend je")
+        break;
+
+    case '7':
+        console.log("Nedelja");
+        console.log("Vikend je")
+        break;
+
+    default:
+        console.log("Niste uneli dobar broj");
+        break;
+
+}
